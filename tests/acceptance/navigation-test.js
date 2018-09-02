ab87c2965c211ad359cb758b264cf8d94d6147be
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-09-01 21:34:04 -0400

    need to figure out how to map the params now

--- tests/acceptance/navigation-test.js
+++ tests/acceptance/navigation-test.js
@@ -5,10 +5,18 @@ import { setupApplicationTest } from 'ember-qunit';
 module('Acceptance | navigation', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /parent/child', async function(assert) {
+  test('visiting parent.child', async function(assert) {
     await visit('/');
     await click('[data-test-parent-child-link]');
 
     assert.equal(currentURL(), '/child');
   });
+
+  test('visiting dynamic/:id', async function(assert) {
+    await visit('/');
+    await click('[data-test-dynamic-with-id');
+
+    assert.equal(currentURL(), '/dynamic/1');
+    assert.dom('[data-test-id]').containsText('1');
+  })
 });
