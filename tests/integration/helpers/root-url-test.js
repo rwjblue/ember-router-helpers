c2395c470f3a46cbc11d957569b0fc9cb2b99463
Thomas Wang <wangxgwxg@gmail.com>
2019-06-06 23:57:48 -0700

    Implement root-url

---
+++ tests/integration/helpers/root-url-test.js
@@ -0,0 +1,18 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Helper | root-url', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it returns rootURL', async function(assert) {
+    await render(hbs`{{root-url}}`);
+    assert.equal(this.element.textContent.trim(), '/');
+  });
+
+  test('can be used to prefix any href', async function(assert) {
+    await render(hbs`<a href="{{root-url}}profile">Profile</a>`);
+    assert.equal(this.element.querySelector('a').getAttribute('href'), '/profile');
+  });
+});
