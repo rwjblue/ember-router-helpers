829d1b0d9e07903d2068ac45e15cfe420567b708
Camille TJHOA <camille.tjhoa@concordnow.com>
2022-05-09 17:59:29 +0000

    Bump test-app v3.18.0...v3.28.5
    npx ember-cli-update --to 3.28

--- test-app/app/router.js
+++ test-app/app/router.js
@@ -1,13 +1,13 @@
 import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import config from 'test-app/config/environment';
 
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function() {
-  this.route('parent', { path: '/' }, function() {
+Router.map(function () {
+  this.route('parent', { path: '/' }, function () {
     this.route('child');
     this.route('sister');
     this.route('brother');
